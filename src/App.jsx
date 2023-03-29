4e385e29c2d62601719a563e839d114299f3c9b6
YouceF-dz16 <y.aissat@gmail.com>
2023-03-30 00:31:31 +0200

    Afficher des données dans un composant react 03 ok

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-        
-      <div>
-        <PokemonCard/>
-      </div>
-      
+    <>
+    <div className="App">     
+    <div>
+      <PokemonCard/>
+    </div> 
     </div>
+    </>
   )
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,33 @@
-function PokemonCard(){
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
+
+const pokemon = pokemonList[0] 
+
+function PokemonCard () {       
+
     return (
+        
+    
+        <figure>
+         {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}, 
+        
+        <figcaption>{pokemon.name}</figcaption>
 
-    <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="bulbasaur"/>
-    <figcaption>bulbasaur</figcaption>
-</figure>
-
-)}
+    </figure>)
+         
+};
 
 
 
