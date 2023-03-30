22fe3bd051205c155e38e7de5b0f41358ad6bf08
YouceF-dz16 <y.aissat@gmail.com>
2023-03-30 22:40:20 +0200

    fournir une définition complète de la forme pour la prop pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,25 @@
-
-
+import PropTypes from "prop-types"
 
 
 function PokemonCard ({pokemon}) {       
 
     return (
-        
-    
+            
         <figure>
          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
-        
+                 
         <figcaption>{pokemon.name}</figcaption>
 
-    </figure>)
-         
+        </figure>)     
 };
 
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({    
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isOptional,
+    }).isRequired,
+  }
+  
 
 
 export default PokemonCard 
