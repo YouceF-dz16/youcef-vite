8ad6059e597c9f9e99cc2bec17f730eb32c77ed4
YouceF-dz16 <y.aissat@gmail.com>
2023-03-31 22:38:29 +0200

    react 06 ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
-
 
 
 
 function App() {
   
   
-  const pokemonList = [
+  const pokemon = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+
+    const [pokemons, setpokemons] = useState(0);
+    const Suivant = () => {setpokemons (pokemons +1)};
+    const precedent = () => {setpokemons (pokemons -1)};
+      
   return (
     <div>
-         
-      <PokemonCard pokemon ={pokemonList[0]}  />
+
+      <PokemonCard pokemon ={pokemon[pokemons]}  />
+      {pokemons > 0 ? <button onClick={precedent}>Précedent</button> : ""};
+      {pokemons < pokemon.length -1 ? <button onClick={Suivant}>Suivant</button>: ""} ;   
     
     </div>
  
-  )
+  );
 }
 
 export default App
